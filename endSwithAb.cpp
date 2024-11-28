785f7b2773a748edb2d739b61cbcfbfa8103a94e
Tamal Mazumder <tamaltm143@gmail.com>
2024-11-28 21:03:32 +0600

    2nd

--- endSwithAb.cpp
+++ endSwithAb.cpp
@@ -2,13 +2,25 @@
 using namespace std;
 
 int main(){
+    int state =0;
     string s;
     cin >> s;
     int n = s.size();
-    if(s[n-1]=='b' && s[n-2]=='a'){
-        cout << "Accepted" << '\n';
-        return 0;
+    cout << "q" << state << "->";
+    if(s[n-2]=='a'){
+        if(state<2)
+        state++;
+    }else{
+        state=0;
     }
-
-    cout << "Rejected" << endl;
+    cout << "q" << state << "->";
+    if(s[n-1]=='b'){
+        if(state<2)
+        state++;
+    }else{
+        state=1;
+    }
+    cout << "q" << state  << endl;
+    if(state==2)cout << "accepted" << '\n';
+    else cout << "rejected" << '\n';
 }
